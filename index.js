7e25db30308922f3f6c02c62ab23786790b58c2a
Daniel Hudson <LuckyLlama72@gmail.com>
2024-06-16 14:07:50 -0400

    Installed items, prepared to do the thing.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Use here to actually run code, others for exports.
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Export a function for some bullshit lmao
+
+const questions = [{
+    type: "input",
+    message: "Enter logo text: ",
+    name: "logoText",
+}, {
+    type: "list",
+    message: "Choose a text color: ",
+    name: "textColor",
+    choices: ["Black", "Blue", "Green", "Red", "White"],
+}, {
+    type: "list",
+    message: "Choose a logo shape: ",
+    name: "logoColor",
+    choices: ["Circle", "Square", "Triangle"],
+},{
+    type: "list",
+    message: "Choose a logo color: ",
+    name: "logoColor",
+    choices: ["Black", "Blue", "Green", "Red", "White"], 
+}]
